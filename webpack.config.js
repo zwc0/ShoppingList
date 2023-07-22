d0ab532879287c41179edef2c02481535515a8d3
Austin S <acceptance364@gmail.com>
2023-07-22 15:33:19 -0700

    Added an import/export feature and mapped the version number to package.json.

--- webpack.config.js
+++ webpack.config.js
@@ -20,4 +20,24 @@ module.exports = [{
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: false
+},{
+  mode: 'development',
+  entry: './src/sw.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'sw.js',
+    path: __dirname,
+  },
+  devtool: false
 }];
