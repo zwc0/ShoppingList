9606a2743d4a6707c9db41a99eabe3afef0d241d
Austin S <acceptance364@gmail.com>
2023-08-08 22:52:58 -0700

    a

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -139,7 +139,7 @@ const Home = () => {
                 const newList = clone(list);
                 const newCurrList = getCurrList(indexArr, newList);
                 newCurrList.splice(index, 0, newCurrList.splice(startIndex, 1)[0]);
-                alert(newList);
+                alert(JSON.stringify(newList));
                 // setList(newList);
                 clear();
             });
