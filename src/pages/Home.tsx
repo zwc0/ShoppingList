2fa402f0f3fc1558660787461f2b5dbd84fd457d
Austin S <acceptance364@gmail.com>
2023-08-13 16:10:52 -0700

    38

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -116,9 +116,10 @@ const Home = () => {
             }
 
             const offTouch = on(document.body, 'touchend', (e)=>{
-                const event = new Event('pointerup');
-                e.target?.dispatchEvent(event);
-                // alert('1');
+                const {target} = e;
+                const item = target instanceof HTMLFormElement ? target : target instanceof HTMLElement ? target.closest('form') : null;
+
+                alert(item?.innerText);
             });
 
             const offUp = on(document.body, 'pointerup', (e)=>{
