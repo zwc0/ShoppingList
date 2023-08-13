3fa09d2775a293dd7b8f2d786b3cbb09ee40910f
Austin S <acceptance364@gmail.com>
2023-08-13 15:10:00 -0700

    t2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -30,7 +30,7 @@ const ListItem =
 
 
     return (
-        <form class={'flex gap-2 items-center ' + className} onSubmit={saveNewTitle}>
+        <form draggable={false} class={'flex gap-2 items-center ' + className} onSubmit={saveNewTitle}>
             <div>
                 <input type="checkbox" checked={done} onChange={()=>onChangeDone({title, done: !done})} />
             </div>
@@ -39,7 +39,7 @@ const ListItem =
 					ref={refInput}
                     value={newTitle}
                     onInput={({currentTarget})=>setNewTitle(currentTarget.value)} />
-                : <div class={`grow ${!done ? '' : 'line-through'}`} onClick={()=>onClickTitle({title})}>
+                : <div draggable={false} class={`grow ${!done ? '' : 'line-through'}`} onClick={()=>onClickTitle({title})}>
                     {title}
                 </div>
             }
@@ -94,7 +94,6 @@ function on<K extends keyof HTMLElementEventMap>(
 }
 const reorderDiffMin = 50;
 const Home = () => {
-    const [force, setForce] = useState(new Date());
     const [list, setList] = useState<TListItem[]>(initList());
 	const [isDark, setIsDark] = useState(tryParseJson(localStorage.getItem('shoppingListDark') || false) || false);
     const [newTitle, setNewTitle] = useState<string>('');
@@ -135,9 +134,6 @@ const Home = () => {
                     });
                     newCurrList.splice(index, 0, newCurrList.splice(startIndex, 1)[0]);
                     console.log({list, newList});
-                    setTimeout(()=>{
-                        setForce(new Date());
-                    }, 1000);
                     return newList;
                 });
                 clear();
@@ -282,7 +278,7 @@ const Home = () => {
 					<path d="M12 22C17.5228 22 22 17.5228 22 12C22 11.5373 21.3065 11.4608 21.0672 11.8568C19.9289 13.7406 17.8615 15 15.5 15C11.9101 15 9 12.0899 9 8.5C9 6.13845 10.2594 4.07105 12.1432 2.93276C12.5392 2.69347 12.4627 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="currentColor"/>
 				</svg>
             </div>
-            <div class="text-sm">v{pkg.version} {force.toString()}</div>
+            <div class="text-sm">v{pkg.version}</div>
             <div>
                 {
                     indexArr.length > 0 && <button type="button"
